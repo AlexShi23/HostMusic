9a805a980508b28bb7ca4b8e1cad771a9dd9695e
Aleksandr Shirokov <alieksandr_shirokov@list.ru>
2022-03-21 22:41:11 +0300

    Added frontend for identity and account management

---
+++ AngularClient/src/app/account/register.component.ts
@@ -0,0 +1,67 @@
+﻿import { Component, Inject, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+import { MustMatch } from '@app/_helpers';
+
+import { AccountService } from '@app/_services';
+import { TuiNotification, TuiNotificationsService } from '@taiga-ui/core';
+
+@Component({ templateUrl: 'register.component.html' })
+export class RegisterComponent implements OnInit {
+    form: FormGroup;
+    loading = false;
+    submitted = false;
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        private accountService: AccountService,
+        @Inject(TuiNotificationsService)
+        private readonly notificationsService: TuiNotificationsService
+    ) { }
+
+    ngOnInit() {
+        this.form = this.formBuilder.group({
+            email: ['', [Validators.required, Validators.email]],
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
+            password: ['', [Validators.required, Validators.minLength(6)]],
+            confirmPassword: ['', Validators.required],
+            acceptTerms: [false, Validators.requiredTrue]
+        }, {
+            validator: MustMatch('password', 'confirmPassword')
+        });
+    }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.form.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+        
+        // stop here if form is invalid
+        if (this.form.invalid) {
+            return;
+        }
+
+        this.loading = true;
+        this.accountService.register(this.form.value)
+            .pipe(first())
+            .subscribe({
+                next: () => {
+                    this.notificationsService.show('Регистрация прошла успешно', {
+                        status: TuiNotification.Success
+                    }).subscribe();
+                    this.router.navigate(['../login'], { relativeTo: this.route });
+                },
+                error: error => { this.notificationsService
+                    .show(error, {
+                        status: TuiNotification.Error
+                    }).subscribe();
+                    this.loading = false;
+                }
+            });
+    }
+}
