06748405ef9a2555ab6c32e81cc5e72b407dafe5
Aleksandr Shirokov <alieksandr_shirokov@list.ru>
2022-03-21 22:14:53 +0300

    Added authentification and authorization with JWT tokens

---
+++ WebApi/Migrations/20220301194511_RemoveTitle.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApi.Migrations
+{
+    public partial class RemoveTitle : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Accounts");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Accounts",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
