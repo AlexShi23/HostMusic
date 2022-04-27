f12469d7777b364802d1e51c33bfd4fd6945ad73
Aleksandr Shirokov <alieksandr_shirokov@list.ru>
2022-04-27 23:55:04 +0300

    add authorization to swagger client

--- Backend/Identity/Identity.App/Startup.cs
+++ Backend/Identity/Identity.App/Startup.cs
@@ -6,6 +6,7 @@ using HostMusic.Identity.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 using SmtpUtils;
 
 namespace HostMusic.Identity.App
@@ -30,7 +31,29 @@ namespace HostMusic.Identity.App
                 x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
             });
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-            services.AddSwaggerGen();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "HostMusic.Identity.API",
+                    Description = "Web API to provide authentication services"
+                });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please insert JWT with Bearer into field",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                    {
+                        new OpenApiSecurityScheme{Reference = new OpenApiReference{Type = ReferenceType.SecurityScheme, Id = "Bearer"}}, Array.Empty<string>()
+                    }
+                });
+            });
 
             services.AddSmtp(Configuration);
             services.AddCore();
