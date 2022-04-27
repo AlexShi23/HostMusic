098d06df0b358c8c83f6e7a015f43efbe46f672a
Aleksandr Shirokov <alieksandr_shirokov@list.ru>
2022-04-27 22:57:42 +0300

    dashboard base version

--- AngularClient/src/app/home/home.component.ts
+++ AngularClient/src/app/home/home.component.ts
@@ -1,10 +1,29 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, ViewChild } from '@angular/core';
+import { Account, Role } from '@app/_models';
 
 import { AccountService } from '@app/_services';
+import { TuiHostedDropdownComponent } from '@taiga-ui/core';
 
-@Component({ templateUrl: 'home.component.html' })
+@Component({ templateUrl: 'home.component.html',
+             styleUrls: ['home.component.less'] })
 export class HomeComponent {
-    account = this.accountService.accountValue;
+    @ViewChild(TuiHostedDropdownComponent)
+    component?: TuiHostedDropdownComponent;
+ 
+    Role = Role;
+    account: Account;
+    menuClosed: boolean;
 
-    constructor(private accountService: AccountService) { }
+    constructor(private accountService: AccountService) {
+        this.accountService.account.subscribe(x => this.account = x);
+        this.menuClosed = true;
+    }
+
+    logout() {
+        this.accountService.logout();
+    }
+
+    changeMenuClosed(): void {
+        this.menuClosed = !this.menuClosed;
+    }
 }
