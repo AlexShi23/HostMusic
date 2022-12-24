6f986eef7ba80182d700df28b6c88c7c8bcf49d6
Alexander Shirokov <alieksandr_shirokov@list.ru>
2022-12-24 21:05:55 +0300

    renaming

---
+++ Frontend/src/app/account/forgot-password/forgot-password.component.ts
@@ -0,0 +1,50 @@
+﻿import { Component, Inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { first, finalize } from 'rxjs/operators';
+
+import { AccountService } from '@app/services';
+import { TuiNotification, TuiNotificationsService } from '@taiga-ui/core';
+
+@Component({ templateUrl: 'forgot-password.component.html' })
+export class ForgotPasswordComponent {
+    loading = false;
+    submitted = false;
+
+    constructor(
+        private accountService: AccountService,
+        @Inject(TuiNotificationsService)
+        private readonly notificationsService: TuiNotificationsService
+    ) { }
+
+    readonly form = new FormGroup({
+        email: new FormControl('', [Validators.required, Validators.email])
+    });
+
+    // convenience getter for easy access to form fields
+    get f() { return this.form.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+        // stop here if form is invalid
+        if (this.form.invalid) {
+            return;
+        }
+
+        this.loading = true;
+        this.accountService.forgotPassword(this.f.email.value)
+            .pipe(first())
+            .pipe(finalize(() => this.loading = false))
+            .subscribe({
+                next: () => this.notificationsService
+                    .show('Инструкция по восстановлению пароля отправлена на ваш email', {
+                        status: TuiNotification.Success
+                    }).subscribe(),
+                error: error => {this.notificationsService
+                    .show(error, {
+                        status: TuiNotification.Error
+                    }).subscribe();
+                    this.loading = false;
+                }
+            });
+    }
+}
