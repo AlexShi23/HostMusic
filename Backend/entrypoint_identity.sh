bbf8d49547d5f003823a4963d4e777e747adf4a5
Aleksandr Shirokov <alieksandr_shirokov@list.ru>
2023-02-18 16:02:14 +0300

    fixes scripts for deploy

--- Backend/entrypoint_identity.sh
+++ Backend/entrypoint_identity.sh
@@ -5,9 +5,9 @@ run_cmd="dotnet run --no-build --project Identity/Identity.App/Identity.App.cspr
 
 export PATH="$PATH:/root/.dotnet/tools"
 
-cd Identity/Identity.Data/
+cd ./Identity/Identity.Data/
 
-until dotnet ef database update; do
+until dotnet ef database update --project=Identity.Data/Identity.Data.csproj --startup-project=Identity.App/Identity.App.csproj --context=IdentityContext; do
     >&2 echo "Migrations executing"
     sleep 1
 done
--- Backend/entrypoint_releases.sh
+++ Backend/entrypoint_releases.sh
@@ -5,9 +5,9 @@ run_cmd="dotnet run --no-build --project Releases/Releases.App/Releases.App.cspr
 
 export PATH="$PATH:/root/.dotnet/tools"
 
-cd Releases/Releases.Data/
+cd ./Releases/Releases.Data/
 
-until dotnet ef database update; do
+until dotnet ef database update --project=Releases.Data/Releases.Data.csproj --startup-project=Releases.App/Releases.App.csproj --context=ReleasesContext; do
     >&2 echo "Migrations executing"
     sleep 1
 done
