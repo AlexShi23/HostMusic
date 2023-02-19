3e6ae9564e5b109bc6f94c93cd41691bf0a58e5d
Aleksandr Shirokov <alieksandr_shirokov@list.ru>
2023-02-19 23:57:56 +0300

    some fixes for prod

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://85.193.81.143:5000',
-  releasesUrl: 'http://85.193.81.143:5283'
+  apiUrl: 'http://hostmusic.ru:5000',
+  releasesUrl: 'http://hostmusic.ru:5283'
 };
